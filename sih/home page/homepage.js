867eb7effcc0409b57113c726465b63686fd0a4f
navneet sagar <130034948+sagar09navneet@users.noreply.github.com>
2023-09-10 21:29:11 +0530

    HOME PAGE AND INDUSTRY PAGE OF BUYRES SECTION .
    POST ALL YOUR CODE HERE

---
+++ sih/home page/homepage.js
@@ -0,0 +1,36 @@
+let slideIndex = 0;
+        showSlides();
+
+        function showSlides() {
+            let slides = document.getElementsByClassName("slide");
+            let dots = document.getElementsByClassName("dot");
+
+            for (let i = 0; i < slides.length; i++) {
+                slides[i].style.display = "none";
+                dots[i].className = dots[i].className.replace(" active-dot", "");
+            }
+
+            slideIndex++;
+
+            if (slideIndex > slides.length) {
+                slideIndex = 1;
+            }
+
+            slides[slideIndex - 1].style.display = "block";
+            dots[slideIndex - 1].className += " active-dot";
+
+            setTimeout(showSlides, 2000); // Change slide every 2 seconds (adjust as needed)
+        }
+        function myFunction() {
+            document.getElementById("myDropdown").classList.toggle("show");
+          }
+          
+          // Close the dropdown if the user clicks outside of it
+          window.onclick = function (e) {
+            if (!(e).target.matches('.dropbtn')) {
+            var myDropdown = document.getElementById("myDropdown");
+              if (myDropdown.classList.contains('show')) {
+                myDropdown.classList.remove('show');
+              }
+            }
+          }
